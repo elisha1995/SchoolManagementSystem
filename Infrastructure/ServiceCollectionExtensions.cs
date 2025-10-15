d6483a40b7077d099da83a8d11c57171076a6a4c
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-15 22:11:16 +0000

    Add `ServiceCollectionExtensions` to register multi-tenancy services in Infrastructure

---
+++ Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+using Infrastructure.Tenancy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services.AddMultiTenancyServices(configuration);
+    }
+}
