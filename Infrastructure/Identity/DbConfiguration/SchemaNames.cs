406d0337c903b62eb415cbaa8ce5dc837d40df62
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-16 23:15:21 +0000

    Add `Academics` schema name to `SchemaNames` for Identity configuration

--- Infrastructure/Identity/DbConfiguration/SchemaNames.cs
+++ Infrastructure/Identity/DbConfiguration/SchemaNames.cs
@@ -3,4 +3,5 @@ namespace Infrastructure.Identity.DbConfiguration;
 internal static class SchemaNames
 {
     public static string Identity = nameof(Identity);
+    public static string Academics = nameof(Academics);
 }
