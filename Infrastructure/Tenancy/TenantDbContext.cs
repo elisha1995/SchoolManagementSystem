07c31576d59762c219dcdbacb2720079e75ff73e
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-17 08:22:37 +0000

    Override `OnModelCreating` in `TenantDbContext` to configure `SchoolTenantInfo` table with multitenancy schema

--- Infrastructure/Tenancy/TenantDbContext.cs
+++ Infrastructure/Tenancy/TenantDbContext.cs
@@ -1,9 +1,16 @@
 using Finbuckle.MultiTenant.EntityFrameworkCore.Stores.EFCoreStore;
+using Infrastructure.Persistence.DbConfiguration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Tenancy;
 
 public class TenantDbContext(DbContextOptions<TenantDbContext> options) : EFCoreStoreDbContext<SchoolTenantInfo>(options)
 {
-    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.Entity<SchoolTenantInfo>()
+            .ToTable("Tenants", SchemaNames.Multitenancy);;
+    }
 }
