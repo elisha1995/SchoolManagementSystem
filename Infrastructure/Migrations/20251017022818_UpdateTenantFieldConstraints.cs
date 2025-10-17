d7bb95e6f76f9b757f4788029b78992085f9cae4
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-17 08:23:58 +0000

    Add initial migrations for TenantDbContext and update Tenant field constraints

---
+++ Infrastructure/Migrations/20251017022818_UpdateTenantFieldConstraints.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTenantFieldConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tenants_Identifier",
+                schema: "Multitenancy",
+                table: "Tenants");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "Multitenancy",
+                table: "Tenants",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Identifier",
+                schema: "Multitenancy",
+                table: "Tenants",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConnectionString",
+                schema: "Multitenancy",
+                table: "Tenants",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdminEmail",
+                schema: "Multitenancy",
+                table: "Tenants",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_Identifier",
+                schema: "Multitenancy",
+                table: "Tenants",
+                column: "Identifier",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tenants_Identifier",
+                schema: "Multitenancy",
+                table: "Tenants");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "Multitenancy",
+                table: "Tenants",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Identifier",
+                schema: "Multitenancy",
+                table: "Tenants",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ConnectionString",
+                schema: "Multitenancy",
+                table: "Tenants",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdminEmail",
+                schema: "Multitenancy",
+                table: "Tenants",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_Identifier",
+                schema: "Multitenancy",
+                table: "Tenants",
+                column: "Identifier",
+                unique: true,
+                filter: "[Identifier] IS NOT NULL");
+        }
+    }
+}
