8657d16d8b63f8787f528aa9ab7a88fe02caf4a5
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-20 23:54:16 +0000

    Call `AddDatabaseInitializerAsync` during application startup to initialize databases

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Infrastructure;
+using Infrastructure.Persistence;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,8 @@ builder.Services.AddOpenApi();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
+
+await app.Services.AddDatabaseInitializerAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
